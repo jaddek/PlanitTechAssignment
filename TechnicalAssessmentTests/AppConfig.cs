b60b9bb1bcf00184cfb15db1a10ae914c7263492
tonynazarov <tonynazarov@gmail.com>
2025-05-29 13:11:25 +1200

    fixed: env var name

--- TechnicalAssessmentTests/AppConfig.cs
+++ TechnicalAssessmentTests/AppConfig.cs
@@ -4,5 +4,5 @@ public static class AppConfig
 {
     public static string Scheme => Environment.GetEnvironmentVariable("SCHEME") ?? "https";
     public static string Host => Environment.GetEnvironmentVariable("HOST") ?? "jupiter.cloud.planittesting.com";
-    public static string PlaywrightTraceDir => Environment.GetEnvironmentVariable("PLAYWRIGHT_TRACE_DIR") ?? "./traces";
+    public static string PlaywrightTraceDir => Environment.GetEnvironmentVariable("PLAYWRIGHT_TRACE_PATH") ?? "./traces";
 }
